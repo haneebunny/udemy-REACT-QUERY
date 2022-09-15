f1764e0d9c931c8bb6d56b390f454cf96abe758d
haneebunny <re____@naver.com>
2022-09-15 23:29:22 +0900

    :sparkles: 스타워즈 끝, 스파 시작

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,22 @@
+import { useQuery } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  const fallback = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+
+  // 데이터를 미리 채우는 방법~
+  // fallback은 대체라는 뜻!
+  // data의 기본값을 fallback으로 해놓으면 잠시 빈 배열이었다가 데이터가 들어온다!
+  return data;
 }
